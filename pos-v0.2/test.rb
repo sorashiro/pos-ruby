c1732c9a483ad4e17243381098f314d097d4da1e
yfwang <yufei930101@gmail.com>
2015-04-27 15:38:36 +0800

    fix test

--- pos-v0.2/test.rb
+++ pos-v0.2/test.rb
@@ -16,7 +16,6 @@ describe Pos do
                ]
 
     it "should print correct text" do
-      pos.print_inventory(inputs)
       expect_text =
         "***<没钱赚商店>购物清单***\n" +
         "名称: 可口可乐, 数量: 5瓶, 单价: 3.00(元), 小计: 15.00(元)\n" +
