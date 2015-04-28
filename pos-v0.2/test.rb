9d1e2a565748144eccd6e0e8934356bb183cf7b2
yfwang <yufei930101@gmail.com>
2015-04-28 10:30:46 +0800

    test

--- pos-v0.2/test.rb
+++ pos-v0.2/test.rb
@@ -1,31 +1,28 @@
 require "minitest/autorun"
-require_relative "pos_v0"
+require_relative "pos"
 
-describe Pos do
-  describe "print_inventory" do
-      pos = Pos.new
-      inputs = [
-                'ITEM000000',
-                'ITEM000000',
-                'ITEM000000',
-                'ITEM000000',
-                'ITEM000000',
-                'ITEM000001',
-                'ITEM000001',
-                'ITEM000004'
-               ]
+describe "print_inventory" do
+    inputs = [
+              'ITEM000000',
+              'ITEM000000',
+              'ITEM000000',
+              'ITEM000000',
+              'ITEM000000',
+              'ITEM000001',
+              'ITEM000001',
+              'ITEM000004'
+             ]
 
-    it "should print correct text" do
-      expect_text =
-        "***<没钱赚商店>购物清单***\n" +
-        "名称: 可口可乐, 数量: 5瓶, 单价: 3.00(元), 小计: 15.00(元)\n" +
-        "名称: 雪碧, 数量: 2瓶, 单价: 3.00(元), 小计: 6.00(元)\n" +
-        "名称: 电池, 数量: 1个, 单价: 2.00(元), 小计: 2.00(元)\n" +
-        "----------------------\n" +
-        "总计: 23.00(元)\n" +
-        "**********************"
+  it "should print correct text" do
+    expect_text =
+      "***<没钱赚商店>购物清单***\n" +
+      "名称: 可口可乐, 数量: 5瓶, 单价: 3.00(元), 小计: 15.00(元)\n" +
+      "名称: 雪碧, 数量: 2瓶, 单价: 3.00(元), 小计: 6.00(元)\n" +
+      "名称: 电池, 数量: 1个, 单价: 2.00(元), 小计: 2.00(元)\n" +
+      "----------------------\n" +
+      "总计: 23.00(元)\n" +
+      "**********************"
 
-      assert_equal(expect_text, pos.print_inventory(inputs))
-    end
+    assert_equal(expect_text, print_inventory(inputs))
   end
 end
