298742c7254848baab7cb6728cc85af59ecfc0df
yfwang <yufei930101@gmail.com>
2015-04-28 10:37:25 +0800

    format

--- pos-v0/test_v0.rb
+++ pos-v0/test_v0.rb
@@ -3,29 +3,29 @@ require_relative "pos_v0"
 
 describe "print_inventory" do
 
-    inputs = [
-      {
-        barcode: "ITEM000000",
-        name: "可口可乐",
-        unit: "瓶",
-        price: 3.00,
-        count: 5
-      },
-      {
-        barcode: "ITEM000001",
-        name: "雪碧",
-        unit: "瓶",
-        price: 3.00,
-        count: 2
-      },
-      {
-        barcode: "ITEM000004",
-        name: "电池",
-        unit: "个",
-        price: 2.00,
-        count: 1
-      }
-    ]
+  inputs = [
+            {
+              barcode: "ITEM000000",
+              name: "可口可乐",
+              unit: "瓶",
+              price: 3.00,
+              count: 5
+            },
+            {
+              barcode: "ITEM000001",
+              name: "雪碧",
+              unit: "瓶",
+              price: 3.00,
+              count: 2
+            },
+            {
+              barcode: "ITEM000004",
+              name: "电池",
+              unit: "个",
+              price: 2.00,
+              count: 1
+            }
+           ]
 
   it "should print correct text" do
     expect_text =
