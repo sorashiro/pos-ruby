423504cb7b9529c47f140eb53319b539703b4570
yfwang <yufei930101@gmail.com>
2015-04-28 10:31:00 +0800

    finish method

---
+++ pos-v0.2/pos.rb
@@ -0,0 +1,97 @@
+def load_all_items
+  return [
+          {
+              barcode: 'ITEM000000',
+              name: '可口可乐',
+              unit: '瓶',
+              price: 3.00
+          },
+          {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00
+          },
+          {
+              barcode: 'ITEM000002',
+              name: '苹果',
+              unit: '斤',
+              price: 5.50
+          },
+          {
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00
+          },
+          {
+              barcode: 'ITEM000004',
+              name: '电池',
+              unit: '个',
+              price: 2.00
+          },
+          {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50
+          }
+         ];
+end
+
+def print_inventory(inputs)
+
+  items = load_all_items
+
+  text = ""
+  total = 0
+  cart_items = []
+
+  inputs.each do |input|
+    items.each do |item|
+      if input == item[:barcode] then
+        cart_items.push(item)
+      end
+    end
+  end
+
+  uniq_cart_items = cart_items.uniq
+
+  cart_items.each do |cart_item|
+    total += cart_item[:price]
+
+    uniq_cart_items.each do |uniq_item|
+      uniq_item[:count] = uniq_item[:count] || 0
+
+      if cart_item[:barcode] == uniq_item[:barcode] then
+        uniq_item[:count] += 1
+      end
+    end
+  end
+
+  uniq_cart_items.each do |item|
+    sub_total = item[:price] * item[:count]
+    text += "名称: #{item[:name]}, 数量: #{item[:count]}#{item[:unit]}, "+
+            "单价: #{format('%.2f', item[:price])}(元), 小计: #{format('%.2f', (item[:price] * item[:count]))}(元)\n"
+  end
+
+  header = "***<没钱赚商店>购物清单***\n"
+  footer = "----------------------\n"+
+           "总计: #{format('%.2f', total)}(元)\n"+
+           "**********************"
+  list = header + text + footer
+  list
+end
+
+inputs = [
+          'ITEM000000',
+          'ITEM000000',
+          'ITEM000000',
+          'ITEM000000',
+          'ITEM000000',
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000004'
+         ]
+
+puts print_inventory(inputs)
