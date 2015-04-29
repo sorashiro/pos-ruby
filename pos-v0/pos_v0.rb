dcf7f67b8a5351c9258c1b5bbb5c649330cfb04b
yfwang <yufei930101@gmail.com>
2015-04-30 01:19:23 +0800

    format

--- pos-v0/pos_v0.rb
+++ pos-v0/pos_v0.rb
@@ -18,25 +18,25 @@ def print_inventory(inputs)
 end
     inputs = [
       {
-        barcode :  "ITEM000000",
-        name    :  "可口可乐",
-        unit    :  "瓶",
-        price   :  3.00,
-        count   :  5
+        barcode: "ITEM000000",
+        name: "可口可乐",
+        unit: "瓶",
+        price: 3.00,
+        count: 5
       },
       {
-        barcode : "ITEM000001",
-        name    : "雪碧",
-        unit    : "瓶",
-        price   : 3.00,
-        count   : 2
+        barcode: "ITEM000001",
+        name: "雪碧",
+        unit: "瓶",
+        price: 3.00,
+        count: 2
       },
       {
-        barcode :  "ITEM000004",
-        name    :  "电池",
-        unit    :  "个",
-        price   :  2.00,
-        count   :  1
+        barcode:  "ITEM000004",
+        name:  "电池",
+        unit:  "个",
+        price:  2.00,
+        count:  1
       }
     ]
 
